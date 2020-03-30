2704ea02ddf80de6fcc109ec555891a811c00fc0
Victor <vmnogueira.ti@gmail.com>
2020-03-30 16:05:54 -0300

    feat(backend): config .env and sequelize postgres using docker

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
   return res.json({
-    ok: true
+    ok: true,
   });
 });
 
